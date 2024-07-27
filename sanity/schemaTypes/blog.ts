4cb9c729424cdca4eac0090aa7ac6c79eba81fba
Azizkhuja <Azizkhuja.Khujaev@Amway.com>
2024-07-27 16:13:17 +0500

    Add schema

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,37 @@
+export default {
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title of blog article',
+    },
+    {
+      name: 'slog',
+      type: 'slog',
+      title: 'Title of slog',
+    },
+    {
+      name: 'titleImage',
+      type: 'image',
+      title: 'Title image',
+    },
+    {
+      name: 'smallDescription',
+      type: 'text',
+      title: 'Small Description',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    },
+  ],
+}
