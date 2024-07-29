4cf32551163b350373bbe98a607c645d2f375068
Azizkhuja <Azizkhuja.Khujaev@Amway.com>
2024-07-29 13:30:52 +0500

    Add event fire for telegram

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,6 +3,16 @@ import { ModeToggle } from "./ModeToggle";
 import { FaTelegram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
+const handleTelegramClick = () => {
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("event", "click", {
+      event_category: "Social Media",
+      event_label: "Telegram Icon",
+      value: "Telegram Click",
+    });
+  }
+};
+
 export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
@@ -11,7 +21,11 @@ export default function Navbar() {
         <span className="text-xs font-extralight">by Azizkhuja</span>
       </Link>
       <div className="flex justify-center items-center">
-        <a href="https://www.linkedin.com/in/azizkhujakhujaev/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/azizkhujakhujaev/"
+          target="_blank"
+          onClick={handleTelegramClick}
+        >
           <FaLinkedin size={28} className="mr-2" />
         </a>
         <a href="https://t.me/devlogsbyazizkhuja" target="_blank">
